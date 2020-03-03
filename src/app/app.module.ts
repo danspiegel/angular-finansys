ebec83771e4083fa505304a40fa88be83e8a13ae
Daniel Spiegel <danispiegel@gmail.com>
2020-03-03 15:17:23 -0300

    Criando interceptador de requisições

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from './in-memory-database';
 
-
 @NgModule({
   declarations: [
     AppComponent
