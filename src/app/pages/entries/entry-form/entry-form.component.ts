5f3bcaf4d612202b2ab03ad8f701b653b7948d90
danspiegel <danispiegel@gmail.com>
2020-04-30 01:52:41 -0300

    Ajustes no form de lançamentos

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -64,7 +64,12 @@ export class EntryFormComponent implements OnInit {
     this.entryForm = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
-      description: [null]
+      description: [null, [Validators.required]],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]]
     });
   }
 
