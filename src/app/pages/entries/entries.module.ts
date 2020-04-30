140c636b6371534b7e0448b80c972e8594470175
danspiegel <danispiegel@gmail.com>
2020-04-30 01:52:41 -0300

    Ajustes no form de lançamentos

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
@@ -12,7 +13,8 @@ import { EntryListComponent } from './entry-list/entry-list.component';
   ],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EntriesModule { }
