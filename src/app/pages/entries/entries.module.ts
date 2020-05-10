e0e47ca5b2bcb2d609618ac5908757a1d93de9bf
danspiegel <danispiegel@gmail.com>
2020-05-10 04:07:54 -0300

    Reestruturação do projeto e seus módulos.

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
@@ -17,9 +16,8 @@ import { IMaskModule } from 'angular-imask';
     EntryFormComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
