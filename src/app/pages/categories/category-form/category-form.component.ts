82457a603cc3acc06c3dcc5708ff2f0e2b08e319
danspiegel <danispiegel@gmail.com>
2020-05-27 02:14:43 -0300

    Criação do BaseResourceFormComponent para ser o componente genérico de form de categoria e lançamentos.

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -14,7 +14,7 @@ import toastr from 'toastr';
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
   currentAction: string;
   categoryForm: FormGroup;
